ea6d8dd28daa4e608bf5a9446f57643036312b0c
AMiXtheySaid <a.fenesan2003@gmail.com>
2025-04-23 16:09:36 +0300

    react is working

---
+++ rprauto.client/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Page from "./loginUI.jsx";
+
+var App = () => {
+    return (
+        <div className="App">
+            <Page />
+        </div>
+    );
+}
+
+export default App;
