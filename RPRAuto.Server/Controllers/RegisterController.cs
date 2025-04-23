20c74413009c23b757967d7e6085e1fca569221f
rares478 <rares478@gmail.com>
2025-04-23 21:00:09 +0300

    Fix CompanyCUI assignment to default to an empty string for non-company registrations

--- RPRAuto.Server/Controllers/RegisterController.cs
+++ RPRAuto.Server/Controllers/RegisterController.cs
@@ -65,7 +65,7 @@ namespace RPRAuto.Server.Controllers
                         Country = ""
                     },
                     Role = registerRequest.IsCompany ? Role.Company : Role.Seller,
-                    CompanyCUI = registerRequest.IsCompany ? registerRequest.CompanyCUI : null,
+                    CompanyCUI = registerRequest.IsCompany ? registerRequest.CompanyCUI : "",
                     Listings = new List<ObjectId>(),
                     Bids = new List<ObjectId>()
                 };
