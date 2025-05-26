ee73c0ebce3176cfeea3d2f9702c57d453a3a9bc
rares478 <rares478@gmail.com>
2025-05-27 00:37:58 +0300

    feat: add JSON property names to Car and ListingCreateRequest models

--- RPRAuto.Server/Models/Car/Car.cs
+++ RPRAuto.Server/Models/Car/Car.cs
@@ -1,47 +1,61 @@
 using MongoDB.Bson.Serialization.Attributes;
 using RPRAuto.Server.Interfaces;
 using RPRAuto.Server.Models.Enums;
+using System.Text.Json.Serialization;
 
 namespace RPRAuto.Server.Models.Car;
 
 public class Car : ICar
 {
     [BsonElement("make")]
+    [JsonPropertyName("Make")]
     public string Make { get; set; } = string.Empty;
 
     [BsonElement("model")]
+    [JsonPropertyName("Model")]
     public string Model { get; set; } = string.Empty;
 
     [BsonElement("year")]
+    [JsonPropertyName("Year")]
     public int Year { get; set; }
 
     [BsonElement("description")]
+    [JsonPropertyName("Description")]
     public string Description { get; set; } = string.Empty;
 
     [BsonElement("gearboxType")]
+    [JsonPropertyName("GearboxType")]
     public GearboxType GearboxType { get; set; }
 
     [BsonElement("color")]
+    [JsonPropertyName("Color")]
     public string Color { get; set; } = string.Empty;
 
     [BsonElement("doors")]
+    [JsonPropertyName("Doors")]
     public int Doors { get; set; }
 
     [BsonElement("fuelType")]
+    [JsonPropertyName("FuelType")]
     public FuelType FuelType { get; set; }
 
     [BsonElement("engineSize")]
+    [JsonPropertyName("EngineSize")]
     public float EngineSize { get; set; }
 
     [BsonElement("bodyType")]
+    [JsonPropertyName("BodyType")]
     public BodyType BodyType { get; set; }
 
     [BsonElement("horsePower")]
+    [JsonPropertyName("HorsePower")]
     public int HorsePower { get; set; }
 
     [BsonElement("mileage")]
+    [JsonPropertyName("Mileage")]
     public int Mileage { get; set; }
 
     [BsonElement("pictures")]
+    [JsonPropertyName("Pictures")]
     public List<string> Pictures { get; set; } = new();
 } 
--- RPRAuto.Server/Models/Listing/ListingCreateRequest.cs
+++ RPRAuto.Server/Models/Listing/ListingCreateRequest.cs
@@ -1,15 +1,29 @@
 using MongoDB.Bson;
 using RPRAuto.Server.Models.Car;
+using System.Text.Json.Serialization;
 
 namespace RPRAuto.Server.Models.Listing;
 
 public class ListingCreateRequest
 {
+    [JsonPropertyName("UserId")]
     public ObjectId UserId { get; set; }
+
+    [JsonPropertyName("Description")]
     public string Description { get; set; } = string.Empty;
+
+    [JsonPropertyName("Status")]
     public Enums.ListingStatus Status { get; set; } = Enums.ListingStatus.Active;
+
+    [JsonPropertyName("Price")]
     public decimal Price { get; set; }
+
+    [JsonPropertyName("Title")]
     public string Title { get; set; } = string.Empty;
+
+    [JsonPropertyName("Car")]
     public Car.Car Car { get; set; } = new Car.Car();
+
+    [JsonPropertyName("EndAt")]
     public DateTime EndAt { get; set; }
 } 
