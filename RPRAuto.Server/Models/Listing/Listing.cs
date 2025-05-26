962ff57ce7974a887a531ce34a0a97bb829e1583
rares478 <rares478@gmail.com>
2025-05-27 00:43:48 +0300

    feat: implement custom JSON converter for Car model and enhance property naming in Listing model

--- RPRAuto.Server/Models/Listing/Listing.cs
+++ RPRAuto.Server/Models/Listing/Listing.cs
@@ -2,6 +2,7 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using RPRAuto.Server.Interfaces;
 using RPRAuto.Server.Models.Car;
+using System.Text.Json.Serialization;
 
 namespace RPRAuto.Server.Models.Listing;
 
@@ -9,27 +10,34 @@ public class Listing : IListing
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
+    [JsonPropertyName("Id")]
     public ObjectId Id { get; set; }
 
     [BsonElement("userId")]
     [BsonRepresentation(BsonType.ObjectId)]
+    [JsonPropertyName("UserId")]
     public ObjectId UserId { get; set; }
 
     [BsonElement("title")]
+    [JsonPropertyName("Title")]
     public string Title { get; set; } = string.Empty;
 
     [BsonElement("price")]
+    [JsonPropertyName("Price")]
     public decimal Price { get; set; }
 
     [BsonElement("car")]
     public ICar Car { get; set; } = new Car.Car();
     
     [BsonElement("status")]
+    [JsonPropertyName("Status")]
     public Enums.ListingStatus Status { get; set; } = Enums.ListingStatus.Active;
     
     [BsonElement("description")]
+    [JsonPropertyName("Description")]
     public string Description { get; set; } = string.Empty;
 
     [BsonElement("createdAt")]
+    [JsonPropertyName("CreatedAt")]
     public DateTime CreatedAt { get; set; }
 } 
